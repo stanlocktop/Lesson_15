2f82c03a2cf04b9b650113317796f1355d213553
stanlocktop <wlad-test@ukr.net>
2023-10-22 19:05:17 +0300

    Create new Project

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Car myCar = new Car(0, 0);
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("Початкова позиція:");
+        Console.WriteLine($"X: {myCar.Position.X}, Y: {myCar.Position.Y}");
+
+        myCar.StartEngine();
+        myCar.Move(10, 20);
+        Console.WriteLine("Автомобіль переміщено:");
+        Console.WriteLine($"X: {myCar.Position.X}, Y: {myCar.Position.Y}");
+
+        myCar.StopEngine();
+        Console.ReadKey();
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int a = 10;
+            int b = 5;
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine($"Додавання: {a} + {b} = {MathUtility.Додавання(a, b)}");
+            Console.WriteLine($"Віднімання: {a} - {b} = {MathUtility.Віднімання(a, b)}");
+            Console.WriteLine($"Множення: {a} * {b} = {MathUtility.Множення(a, b)}");
+            int c = 10;
+            int d = 0;
+            Console.WriteLine($"Ділення: {c} / {d} = {MathUtility.Ділення(c, d)}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.ReadKey();
+        }
+    }
+}
